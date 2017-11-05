a6e6cae0ded83edf75dc10eb31837299eb1dce98
Jyoti Duwal <duwaljyoti16@gmail.com>
2017-11-05 19:09:42 +0545

    created minimum required database migrations for adding products

--- database/migrations/2017_11_05_035844_create_prices_table.php
+++ database/migrations/2017_11_05_035844_create_prices_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreatePricesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('prices', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('is_admin')->default(false);
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('product_variant_id')->unsigned();
+            $table->foreign('product_variant_id')->references('id')->on('product_variants');
+            $table->integer('discount_amount');
             $table->timestamps();
         });
     }
@@ -31,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('prices');
     }
 }
